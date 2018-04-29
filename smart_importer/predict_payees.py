beancount/smart_importer

commit edee35ee8a9c35a8f68c01cc6a892c866425623b

Decorators can now be applied to importer classes or extract methods, as preferred by the user. And they play nice with fava.

smart_importer/predict_payees.py
@@ -3,7 +3,7 @@ Decorator for a Beancount Importers's `extract` function
 that suggests and predicts payees
 using machine learning.
 """
-
+import inspect
 import logging
 from functools import wraps
 from typing import List, Union
@@ -17,22 +17,25 @@ from sklearn.svm import SVC
 
 from smart_importer import machinelearning_helpers as ml
 
+# configure logging
+LOG_LEVEL = logging.DEBUG
+logging.basicConfig(level=LOG_LEVEL)
 logger = logging.getLogger(__name__)
 
 
 class PredictPayees:
     '''
-    Applying this decorator to the extract function of a beancount importer
-    will predict and auto-complete missing payees
-    of the transactions that are imported.
+    Applying this decorator to an importer class or its extract method
+    will use machine learning to learn and predict missing payees
+    for the transactions that are imported.
 
     Predictions are implemented using machine learning
     based on training data read from a beancount file.
 
     Example:
 
+    @PredictPayees(training_data="trainingdata.beancount")
     class MyImporter(ImporterProtocol):
-        @PredictPayees(training_data="trainingdata.beancount")
         def extract(file):
           # do the import, return list of entries
     '''
@@ -52,103 +55,111 @@ class PredictPayees:
         self.overwrite_existing_payees = overwrite_existing_payees
         self.suggest_payees = suggest_payees
 
-    def __call__(self, importers_extract_function, *args, **kwargs):
-        # Decorating the extract function:
+    def __call__(self, to_be_decorated=None, *args, **kwargs):
 
-        @wraps(importers_extract_function)
-        def _extract(importerInstance: ImporterProtocol, *args, **kwargs) -> List[Transaction]:
-            """
-            Completes missing payees using machine learning.
-            :param importerInstance: refers to the importer object, which is normally passed in
-                as `self` argument.
-            :param *args: original arguments to be passed
-            :param **kwargs: original keyword arguments to be passed
-            :return: list of beancount transactions
-            """
+        if inspect.isclass(to_be_decorated):
+            logger.debug('The Decorator was applied to a class.')
+            return self.patched_importer_class(to_be_decorated)
 
-            existing_entries = None
-            if 'existing_entries' in kwargs:
-                existing_entries = kwargs['existing_entries']
+        elif inspect.isfunction(to_be_decorated):
+            logger.debug('The Decorator was applied to an instancemethod.')
+            return self.patched_extract_function(to_be_decorated)
 
-            # load training data
-            self.training_data = ml.load_training_data(
-                self.training_data,
-                filter_training_data_by_account=self.filter_training_data_by_account,
-                existing_entries=existing_entries)
+    def patched_importer_class(self, importer_class):
+        importer_class.extract = self.patched_extract_function(importer_class.extract)
+        return importer_class
 
-            # train the machine learning model
-            self._trained = False
-            if not self.training_data:
-                logger.warning("Cannot train the machine learning model "
-                               "because the training data is empty.")
-            elif len(self.training_data) < 2:
-                logger.warning("Cannot train the machine learning model "
-                               "because the training data consists of less than two elements.")
+    def patched_extract_function(self, original_extract_function):
+        decorator = self
+
+        @wraps(original_extract_function)
+        def wrapper(self, file, existing_entries=None):
+            decorator.existing_entries = existing_entries
+
+            logger.debug(f"About to call the importer's extract function to receive entries to be imported...")
+            if 'existing_entries' in inspect.signature(original_extract_function).parameters:
+                decorator.imported_transactions = original_extract_function(self, file, existing_entries)
             else:
-                self.pipeline = Pipeline([
-                    ('union', FeatureUnion(
-                        transformer_list=[
-                            ('narration', Pipeline([
-                                ('getNarration', ml.GetNarration()),
-                                ('vect', CountVectorizer(ngram_range=(1, 3))),
-                            ])),
-                            ('payee', Pipeline([  # any existing payee, if one exists
-                                ('getPayee', ml.GetPayee()),
-                                ('vect', CountVectorizer(ngram_range=(1, 3))),
-                            ])),
-                            ('dayOfMonth', Pipeline([
-                                ('getDayOfMonth', ml.GetDayOfMonth()),
-                                ('caster', ml.ArrayCaster()),  # need for issue with data shape
-                            ])),
-                        ],
-                        transformer_weights={
-                            'narration': 0.8,
-                            'payee': 0.5,
-                            'dayOfMonth': 0.1
-                        })),
-                    ('svc', SVC(kernel='linear')),
-                ])
-                logger.debug("About to train the machine learning model...")
-                self.pipeline.fit(self.training_data, ml.GetPayee().transform(self.training_data))
-                logger.info("Finished training the machine learning model.")
-                self._trained = True
+                decorator.imported_transactions = original_extract_function(self, file)
 
-            # import transactions by calling the importer's extract function
-            logger.debug(f"About to call the importer's extract function in order to receive entries to be imported...")
-            transactions: List[Transaction]
-            transactions = importers_extract_function(importerInstance, *args, **kwargs)
-            logger.debug(f"Received {len(transactions)} entries by calling the importer's extract function.")
+            return decorator.enhance_transactions()
 
-            if not self._trained:
-                logger.warning("Cannot generate predictions or suggestions "
-                               "because there is no trained machine learning model.")
-                return transactions
+        return wrapper
 
-            # predict payees
-            if self.predict_payees:
-                logger.debug("About to generate predictions for payees...")
-                predicted_payees: List[str]
-                predicted_payees = self.pipeline.predict(transactions)
-                transactions = [ml.add_payee_to_transaction(*t_p, overwrite=self.overwrite_existing_payees)
-                                for t_p in zip(transactions, predicted_payees)]
-                logger.debug("Finished adding predicted payees to the transactions to be imported.")
+    def enhance_transactions(self):
+        # load training data
+        self.training_data = ml.load_training_data(
+            self.training_data,
+            filter_training_data_by_account=self.filter_training_data_by_account,
+            existing_entries=self.existing_entries)
 
-            # suggest likely payees
-            if self.suggest_payees:
-                # get values from the SVC decision function
-                logger.debug("About to generate suggestions about likely payees...")
-                decision_values = self.pipeline.decision_function(transactions)
+        # train the machine learning model
+        self._trained = False
+        if not self.training_data:
+            logger.warning("Cannot train the machine learning model "
+                           "because the training data is empty.")
+        elif len(self.training_data) < 2:
+            logger.warning("Cannot train the machine learning model "
+                           "because the training data consists of less than two elements.")
+        else:
+            self.pipeline = Pipeline([
+                ('union', FeatureUnion(
+                    transformer_list=[
+                        ('narration', Pipeline([
+                            ('getNarration', ml.GetNarration()),
+                            ('vect', CountVectorizer(ngram_range=(1, 3))),
+                        ])),
+                        ('payee', Pipeline([  # any existing payee, if one exists
+                            ('getPayee', ml.GetPayee()),
+                            ('vect', CountVectorizer(ngram_range=(1, 3))),
+                        ])),
+                        ('dayOfMonth', Pipeline([
+                            ('getDayOfMonth', ml.GetDayOfMonth()),
+                            ('caster', ml.ArrayCaster()),  # need for issue with data shape
+                        ])),
+                    ],
+                    transformer_weights={
+                        'narration': 0.8,
+                        'payee': 0.5,
+                        'dayOfMonth': 0.1
+                    })),
+                ('svc', SVC(kernel='linear')),
+            ])
+            logger.debug("About to train the machine learning model...")
+            self.pipeline.fit(self.training_data, ml.GetPayee().transform(self.training_data))
+            logger.info("Finished training the machine learning model.")
+            self._trained = True
 
-                # add a human-readable class label (i.e., payee's name) to each value, and sort by value:
-                suggested_payees = [[payee for _, payee in sorted(list(zip(distance_values, self.pipeline.classes_)),
-                                                                  key=lambda x: x[0], reverse=True)]
-                                    for distance_values in decision_values]
+        if not self._trained:
+            logger.warning("Cannot generate predictions or suggestions "
+                           "because there is no trained machine learning model.")
+            return self.imported_transactions
 
-                # add the suggested payees to each transaction:
-                transactions = [ml.add_suggested_payees_to_transaction(*t_p)
-                                for t_p in zip(transactions, suggested_payees)]
-                logger.debug("Finished adding suggested payees to the transactions to be imported.")
+        # predict payees
+        self.transactions = self.imported_transactions
+        if self.predict_payees:
+            logger.debug("About to generate predictions for payees...")
+            predicted_payees: List[str]
+            predicted_payees = self.pipeline.predict(self.transactions)
+            self.transactions = [ml.add_payee_to_transaction(*t_p, overwrite=self.overwrite_existing_payees)
+                            for t_p in zip(self.transactions, predicted_payees)]
+            logger.debug("Finished adding predicted payees to the transactions to be imported.")
 
-            return transactions
+        # suggest likely payees
+        if self.suggest_payees:
+            # get values from the SVC decision function
+            logger.debug("About to generate suggestions about likely payees...")
+            decision_values = self.pipeline.decision_function(self.imported_transactions)
 
-        return _extract
+            # add a human-readable class label (i.e., payee's name) to each value, and sort by value:
+            suggested_payees = [[payee for _, payee in sorted(list(zip(distance_values, self.pipeline.classes_)),
+                                                              key=lambda x: x[0], reverse=True)]
+                                for distance_values in decision_values]
+
+            # add the suggested payees to each transaction:
+            self.transactions = [ml.add_suggested_payees_to_transaction(*t_p)
+                            for t_p in zip(self.transactions, suggested_payees)]
+            logger.debug("Finished adding suggested payees to the transactions to be imported.")
+
+        return self.transactions
+

smart_importer/examples/importer_using_predict_postings/tests/importer_using_predict_postings_test.py
@@ -23,12 +23,13 @@ class PredictPostingsTest(unittest.TestCase):
     '''
 
     def test_the_importer(self):
+        logger.info("Running Test Case: {id}".format(id=self.id().split('.')[-1]))
         importer = Importer(['.*'])
         mocked_file = Mock(spec=_FileMemo)
         mocked_file.name = 'downloaded-transactions.csv'
         mocked_file.contents = Mock(name='contents', return_value='')
         entries = importer.extract(mocked_file, existing_entries=self.training_data)
-        printer.print_entries(entries)
+        # printer.print_entries(entries)
 
 
 if __name__ == '__main__':

smart_importer/predict_postings.py
@@ -3,8 +3,9 @@ Decorator for Beancount Importer classes
 that suggests and predicts postings using machine learning.
 """
 
-import logging
 import inspect
+import logging
+from functools import wraps
 from typing import List, Union
 
 from beancount.core.data import TxnPosting, Transaction
@@ -15,25 +16,27 @@ from sklearn.svm import SVC
 
 from smart_importer import machinelearning_helpers as ml
 
+# configure logging
+LOG_LEVEL = logging.DEBUG
+logging.basicConfig(level=LOG_LEVEL)
 logger = logging.getLogger(__name__)
 
 
+# the decorator
 class PredictPostings:
     '''
     Decorator for beancount importer classes.
 
-    Applying this decorator to the a class inheriting
-    from `beancount.ingest.importer import ImporterProtocol`
-    will predict missing second postings
-    and suggest auto-completions for editing the postings
-    of the transactions that are imported.
-
-    Predictions are implemented using machine learning
-    based on training data read from a beancount file.
+    Applying this decorator to an importer class or its extract method
+    will use machine learning to learn and predict missing second postings
+    for the transactions that are imported.
 
     Example:
 
-    @PredictPostings(training_data="trainingdata.beancount")
+    @PredictPostings(
+        training_data="trainingdata.beancount",
+        filter_training_data_by_account="The:Importers:Already:Known:Accountname"
+    )
     class MyImporter(ImporterProtocol):
         def extract(file):
           # do the import, return list of entries
@@ -45,7 +48,8 @@ class PredictPostings:
     # https://andrefsp.wordpress.com/2012/08/23/writing-a-class-decorator-in-python/
 
     def __init__(
-            self, *,
+            self,
+            *,
             training_data: Union[_FileMemo, List[Transaction], str] = None,
             filter_training_data_by_account: str = None,
             predict_second_posting: bool = True,
@@ -56,31 +60,44 @@ class PredictPostings:
         self.predict_second_posting = predict_second_posting
         self.suggest_accounts = suggest_accounts
 
-    def __call__(self, OriginalImporter):
+    def __call__(self, to_be_decorated=None, *args, **kwargs):
+
+        if inspect.isclass(to_be_decorated):
+            logger.debug('The Decorator was applied to a class.')
+            return self.patched_importer_class(to_be_decorated)
+
+        elif inspect.isfunction(to_be_decorated):
+            logger.debug('The Decorator was applied to an instancemethod.')
+            return self.patched_extract_function(to_be_decorated)
+
+    def patched_importer_class(self, importer_class):
+        importer_class.extract = self.patched_extract_function(importer_class.extract)
+        return importer_class
+
+    def patched_extract_function(self, original_extract_function):
         decorator = self
 
-        class PredictPostingsImporter(OriginalImporter):
-            def extract(self, file, existing_entries=None):
-                logger.debug(f"About to call the importer's extract function to receive entries to be imported...")
-                if 'existing_entries' in inspect.signature(super().extract).parameters:
-                    decorator.imported_transactions = super().extract(file, existing_entries)
-                else:
-                    decorator.imported_transactions = super().extract(file)
-                logger.debug(
-                    f"Received {len(decorator.imported_transactions)} entries by calling the importer's extract function.")
-                return decorator._extract(
-                    file,
-                    existing_entries
-                )
+        @wraps(original_extract_function)
+        def wrapper(self, file, existing_entries=None):
+            decorator.existing_entries = existing_entries
 
-        return PredictPostingsImporter
+            logger.debug(f"About to call the importer's extract function to receive entries to be imported...")
+            if 'existing_entries' in inspect.signature(original_extract_function).parameters:
+                decorator.imported_transactions = original_extract_function(self, file, existing_entries)
+            else:
+                decorator.imported_transactions = original_extract_function(self, file)
 
-    def _extract(self, file, existing_entries):
+
+            return decorator.enhance_transactions()
+
+        return wrapper
+
+    def enhance_transactions(self):
         # load training data
         self.training_data = ml.load_training_data(
             self.training_data,
             filter_training_data_by_account=self.filter_training_data_by_account,
-            existing_entries=existing_entries)
+            existing_entries=self.existing_entries)
 
         # convert training data to a list of TxnPostings
         self.converted_training_data = [TxnPosting(t, p) for t in self.training_data for p in t.postings

commit b6e99a971cac780d4bd73bceb4f60d37c04935f9

Remove duplicate code caused by merge
Remove duplicate code caused by commit edee35e ("Merge branch 'master'
into decorator-for-importer-classes")

smart_importer/predict_payees.py
@@ -142,15 +142,6 @@ class PredictPayees:
             self.transactions = [ml.add_payee_to_transaction(*t_p, overwrite=self.overwrite_existing_payees)
                             for t_p in zip(self.transactions, predicted_payees)]
             logger.debug("Finished adding predicted payees to the transactions to be imported.")
-        # predict payees
-        self.transactions = self.imported_transactions
-        if self.predict_payees:
-            logger.debug("About to generate predictions for payees...")
-            predicted_payees: List[str]
-            predicted_payees = self.pipeline.predict(self.imported_transactions)
-            self.transactions = [ml.add_payee_to_transaction(*t_p, overwrite=self.overwrite_existing_payees)
-                            for t_p in zip(self.imported_transactions, predicted_payees)]
-            logger.debug("Finished adding predicted payees to the transactions to be imported.")
 
         # suggest likely payees
         if self.suggest_payees: