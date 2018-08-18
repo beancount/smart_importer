a2a3690817fd1cc4f7cb0df43427a89a65242e18
Johannes Harms <johannesjh@users.noreply.github.com>
2018-08-18 20:59:45 +0200

    Splits the enhance_transactions method into multiple smaller methods to ease overwriting. NOTE AND WARNING: This is work in progress. The smaller methods should assert that all preconditions are fulfilled and the enhance_transactions method should handle according exceptions.

--- smart_importer/predict_postings.py
+++ smart_importer/predict_postings.py
@@ -57,24 +57,12 @@ class PredictPostings(SmartImporterDecorator):
         self.account = account
         self.predict_second_posting = predict_second_posting
         self.suggest_accounts = suggest_accounts
-
+        self._trained = False
 
     def enhance_transactions(self):
-        # load training data
-        self.training_data = ml.load_training_data(
-            self.training_data,
-            known_account=self.account,
-            existing_entries=self.existing_entries)
+        self.load_training_data()
+        self.prepare_training_data()
 
-        # convert training data to a list of TxnPostingAccounts
-        self.converted_training_data = [ml.TxnPostingAccount(t, p, pRef.account)
-                                        for t in self.training_data
-                                        for pRef in t.postings
-                                        for p in t.postings
-                                        if p.account != pRef.account]
-
-        # train the machine learning model
-        self._trained = False
         if not self.converted_training_data:
             logger.warning("Cannot train the machine learning model "
                            "because the training data is empty.")
@@ -82,47 +70,7 @@ class PredictPostings(SmartImporterDecorator):
             logger.warning("Cannot train the machine learning model "
                            "because the training data consists of less than two elements.")
         else:
-            transformers = []
-            transformer_weights = {}
-            transformers.append(
-                ('narration', Pipeline([
-                    ('getNarration', ml.GetNarration()),
-                    ('vect', CountVectorizer(ngram_range=(1, 3))),
-                ]))
-            )
-            transformer_weights['narration'] = 0.8
-            transformers.append(
-                ('account', Pipeline([
-                    ('getReferencePostingAccount', ml.GetReferencePostingAccount()),
-                    ('vect', CountVectorizer(ngram_range=(1, 3))),
-                ]))
-            )
-            transformer_weights['account'] = 0.8
-
-            distinctPayees = set(map(lambda trx: trx.txn.payee, self.converted_training_data))
-            if len(distinctPayees) > 1:
-                transformers.append(
-                    ('payee', Pipeline([
-                        ('getPayee', ml.GetPayee()),
-                        ('vect', CountVectorizer(ngram_range=(1, 3))),
-                    ]))
-                )
-                transformer_weights['payee'] = 0.5
-
-            transformers.append(
-                ('dayOfMonth', Pipeline([
-                    ('getDayOfMonth', ml.GetDayOfMonth()),
-                    ('caster', ml.ArrayCaster()),  # need for issue with data shape
-                ]))
-            )
-            transformer_weights['dayOfMonth'] = 0.1
-
-            self.pipeline = Pipeline([
-                ('union', FeatureUnion(
-                    transformer_list=transformers,
-                    transformer_weights=transformer_weights)),
-                ('svc', SVC(kernel='linear')),
-            ])
+            self.define_pipeline()
             logger.debug("About to train the machine learning model...")
             self.pipeline.fit(self.converted_training_data,
                               ml.GetPostingAccount().transform(self.converted_training_data))
@@ -134,20 +82,74 @@ class PredictPostings(SmartImporterDecorator):
                            "because there is no trained machine learning model.")
             return self.imported_entries
 
+        return self.process_transactions()
+
+    def load_training_data(self):
+        self.training_data = ml.load_training_data(
+            self.training_data,
+            known_account=self.account,
+            existing_entries=self.existing_entries)
+
+    def prepare_training_data(self):
+        self.converted_training_data = [ml.TxnPostingAccount(t, p, pRef.account)
+                                        for t in self.training_data
+                                        for pRef in t.postings
+                                        for p in t.postings
+                                        if p.account != pRef.account]
+
+    def define_pipeline(self):
+        transformers = []
+        transformer_weights = {}
+        transformers.append(
+            ('narration', Pipeline([
+                ('getNarration', ml.GetNarration()),
+                ('vect', CountVectorizer(ngram_range=(1, 3))),
+            ]))
+        )
+        transformer_weights['narration'] = 0.8
+        transformers.append(
+            ('account', Pipeline([
+                ('getReferencePostingAccount', ml.GetReferencePostingAccount()),
+                ('vect', CountVectorizer(ngram_range=(1, 3))),
+            ]))
+        )
+        transformer_weights['account'] = 0.8
+        distinctPayees = set(map(lambda trx: trx.txn.payee, self.converted_training_data))
+        if len(distinctPayees) > 1:
+            transformers.append(
+                ('payee', Pipeline([
+                    ('getPayee', ml.GetPayee()),
+                    ('vect', CountVectorizer(ngram_range=(1, 3))),
+                ]))
+            )
+            transformer_weights['payee'] = 0.5
+        transformers.append(
+            ('dayOfMonth', Pipeline([
+                ('getDayOfMonth', ml.GetDayOfMonth()),
+                ('caster', ml.ArrayCaster()),  # need for issue with data shape
+            ]))
+        )
+        transformer_weights['dayOfMonth'] = 0.1
+        self.pipeline = Pipeline([
+            ('union', FeatureUnion(
+                transformer_list=transformers,
+                transformer_weights=transformer_weights)),
+            ('svc', SVC(kernel='linear')),
+        ])
+
+    def process_transactions(self):
         imported_transactions: List[Transaction]
         imported_transactions = list(filter_txns(self.imported_entries))
         enhanced_transactions: List[Transaction]
         enhanced_transactions = list(imported_transactions)
-
         # predict missing second postings
         if self.predict_second_posting:
             logger.debug("About to generate predictions for missing second postings...")
             predicted_accounts: List[str]
             predicted_accounts = self.pipeline.predict(enhanced_transactions)
             enhanced_transactions = [ml.add_posting_to_transaction(*t_a)
-                                 for t_a in zip(enhanced_transactions, predicted_accounts)]
+                                     for t_a in zip(enhanced_transactions, predicted_accounts)]
             logger.debug("Finished adding predicted accounts to the transactions to be imported.")
-
         # suggest accounts that are likely involved in the transaction
         if self.suggest_accounts:
             # get values from the SVC decision function
@@ -161,7 +163,6 @@ class PredictPostings(SmartImporterDecorator):
 
             # add the suggested accounts to each transaction:
             enhanced_transactions = [ml.add_suggested_accounts_to_transaction(*t_s)
-                                 for t_s in zip(enhanced_transactions, suggestions)]
+                                     for t_s in zip(enhanced_transactions, suggestions)]
             logger.debug("Finished adding suggested accounts to the transactions to be imported.")
-
         return ml.merge_non_transaction_entries(self.imported_entries, enhanced_transactions)
