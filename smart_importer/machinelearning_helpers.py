4c2ec94ebdb8ed990828d358fedb8db5eb3ea08b
Jakob Schnitzer <mail@jakobschnitzer.de>
2018-09-06 12:23:44 +0200

    remove dead code

--- smart_importer/machinelearning_helpers.py
+++ smart_importer/machinelearning_helpers.py
@@ -118,82 +118,15 @@ TxnPostingAccount = NamedTuple('TxnPostingAccount',
                                 ('account', str)])
 
 
-class ItemSelector(BaseEstimator, TransformerMixin):
-    """
-    Helper class:
-    For data grouped by feature, select subset of data at a provided key.
-
-    Code from:
-    http://scikit-learn.org/stable/auto_examples/hetero_feature_union.html
-
-    The data is expected to be stored in a 2D data structure, where the first
-    index is over features and the second is over samples.  i.e.
-
-    >> len(data[key]) == n_samples
-
-    Please note that this is the opposite convention to scikit-learn feature
-    matrixes (where the first index corresponds to sample).
-
-    ItemSelector only requires that the collection implement getitem
-    (data[key]).  Examples include: a dict of lists, 2D numpy array, Pandas
-    DataFrame, numpy record array, etc.
-
-    >> data = {'a': [1, 5, 2, 5, 2, 8],
-               'b': [9, 4, 1, 4, 1, 3]}
-    >> ds = ItemSelector(key='a')
-    >> data['a'] == ds.transform(data)
-
-    ItemSelector is not designed to handle data grouped by sample.  (e.g. a
-    list of dicts).  If your data is structured this way, consider a
-    transformer along the lines of `sklearn.feature_extraction.DictVectorizer`.
-
-    Parameters
-    ----------
-    key : hashable, required
-        The key corresponding to the desired value in a mappable.
-    """
-
-    def __init__(self, key):
-        self.key = key
-
-    def fit(self, x, y=None):
-        return self
-
-    def transform(self, data_dict):
-        return data_dict[self.key]
-
-
-class StatusPrinter(BaseEstimator, TransformerMixin):
-    """
-    Helper class to print data that is passed through a scikit-learn pipeline.
-    """
-
-    def __init__(self):
-        pass
-
-    def fit(self, x, y=None):
-        return self
-
-    def transform(self, data):
-        print(data)
-        return data
-
-
 class ArrayCaster(BaseEstimator, TransformerMixin):
     """
     Helper class for casting data into array shape.
     """
 
-    def __init__(self, debug=False):
-        self.debug = debug
-
     def fit(self, x, y=None):
         return self
 
     def transform(self, data):
-        if self.debug:
-            print(data.shape)
-            print(np.transpose(np.matrix(data)).shape)
         return np.transpose(np.matrix(data))
 
 
