fb1c685af25d4f70934c828dbbac4c36ad3bc142
Patrick Ruckstuhl <patrick@ch.tario.org>
2018-10-07 20:14:01 +0200

    Expose duplicate comparator and window_days
    allows to customize duplicate detection logic

--- smart_importer/detector.py
+++ smart_importer/detector.py
@@ -11,10 +11,17 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 class DuplicateDetector(ImporterDecorator):
     """Class for duplicate detector importer helpers.
+
+    Args:
+        comparator: A functor used to establish the similarity of two entries.
+        window_days: The number of days (inclusive) before or after to scan the
+        entries to classify against.
     """
 
-    def __init__(self):
+    def __init__(self, comparator=None, window_days=2):
         super().__init__()
+        self.comparator = comparator
+        self.window_days = window_days
 
     def main(self, imported_entries, existing_entries):
         """Add duplicate metadata for imported transactions.
@@ -28,7 +35,7 @@ class DuplicateDetector(ImporterDecorator):
             A list of entries, modified by this detector.
         """
 
-        duplicate_pairs = similar.find_similar_entries(imported_entries, existing_entries)
+        duplicate_pairs = similar.find_similar_entries(imported_entries, existing_entries, self.comparator, self.window_days)
         # Add a metadata marker to the extracted entries for duplicates.
         duplicate_set = set(id(entry) for entry, _ in duplicate_pairs)
         mod_entries = []
