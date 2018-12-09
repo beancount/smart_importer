b38fb2f58fb5d782424da25660e47f3824887e47
Johannes Harms <johannesjh@users.noreply.github.com>
2018-12-09 11:26:19 +0100

    refs #41 removes examples.

--- examples/run-bean-extract.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-VENV="/Users/johannes/repos/beancount/virtualenv"
-
-pushd `dirname $0` > /dev/null
-source ${VENV}/bin/activate
-bean-extract main.import.py ./downloads -f ledger/example.beancount
-popd > /dev/null
