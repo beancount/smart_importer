28b71ae8996bc7a70ead8b5f45878bd4e48c538d
EINDEX <eindex.me@outlook.com>
2022-02-06 23:29:36 +0800

    add document for string_tokenizer

--- smart_importer/predictor.py
+++ smart_importer/predictor.py
@@ -3,7 +3,7 @@
 
 import logging
 import threading
-from typing import Dict, List, Optional, Union
+from typing import Callable, Dict, List, Optional, Union
 
 from beancount.core.data import (
     ALL_DIRECTIVES,
@@ -33,6 +33,9 @@ class EntryPredictor(ImporterHook):
         predict: Whether to add predictions to the entries.
         overwrite: When an attribute is predicted but already exists on an
             entry, overwrite the existing one.
+        string_tokenizer: Tokenizer can let smart_importer support more
+            languages. This parameter should be an callable function with
+            string parameter and the returning should be a list.
     """
 
     # pylint: disable=too-many-instance-attributes
@@ -40,7 +43,12 @@ class EntryPredictor(ImporterHook):
     weights: Dict[str, float] = {}
     attribute: Optional[str] = None
 
-    def __init__(self, predict=True, overwrite=False, string_tokenizer=None):
+    def __init__(
+        self,
+        predict=True,
+        overwrite=False,
+        string_tokenizer: Callable[[str], List] = None,
+    ):
         super().__init__()
         self.training_data = None
         self.open_accounts = {}
