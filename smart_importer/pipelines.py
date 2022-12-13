c3ba0643e432eabe9a6f66b01432958d9194e46d
Jakob Schnitzer <mail@jakobschnitzer.de>
2022-12-13 16:50:50 +0100

    support scikit-learn v1.2
    It seems that sklearn did not deal to well with us passing along some
    plain arrays, the first pipeline step should probably directly return a
    numpy array.

--- smart_importer/pipelines.py
+++ smart_importer/pipelines.py
@@ -1,6 +1,8 @@
 """Machine learning pipelines for data extraction."""
+
+from __future__ import annotations
+
 import operator
-from typing import List
 
 import numpy
 from beancount.core.data import Transaction
@@ -17,43 +19,41 @@ class NoFitMixin:
         return self
 
 
-class ArrayCaster(BaseEstimator, TransformerMixin, NoFitMixin):
-    """Helper class for casting data into array shape."""
+def txn_attr_getter(attribute_name: str):
+    """Return attribute getter for a transaction that also handles metadata."""
+    if attribute_name.startswith("meta."):
+        meta_attr = attribute_name[5:]
 
-    @staticmethod
-    def transform(data):
-        """Turn list into numpy array of the necessary shape."""
-        return numpy.array(data, ndmin=2).T
+        def getter(txn):
+            return txn.meta.get(meta_attr)
+
+        return getter
+    return operator.attrgetter(attribute_name)
 
 
-class Getter(TransformerMixin, NoFitMixin):
-    """Get an entry attribute."""
+class NumericTxnAttribute(BaseEstimator, TransformerMixin, NoFitMixin):
+    """Get a numeric transaction attribute and vectorize."""
 
-    def transform(self, data: List[Transaction]):
+    def __init__(self, attr: str):
+        self.attr = attr
+        self._txn_getter = txn_attr_getter(attr)
+
+    def transform(self, data: list[Transaction], _y=None):
         """Return list of entry attributes."""
-        return [self._getter(d) for d in data]
-
-    def _getter(self, txn):
-        raise NotImplementedError
+        return numpy.array([self._txn_getter(d) for d in data], ndmin=2).T
 
 
-class AttrGetter(Getter):
-    """Get a transaction attribute."""
+class AttrGetter(BaseEstimator, TransformerMixin, NoFitMixin):
+    """Get a string transaction attribute."""
 
-    def __init__(self, attr, default=None):
+    def __init__(self, attr: str, default: str | None = None):
+        self.attr = attr
         self.default = default
-        if attr.startswith("meta."):
-            meta_attr = attr[5:]
+        self._txn_getter = txn_attr_getter(attr)
 
-            def getter(txn):
-                return txn.meta.get(meta_attr)
-
-            self._txn_getter = getter
-        else:
-            self._txn_getter = operator.attrgetter(attr)
-
-    def _getter(self, txn):
-        return self._txn_getter(txn) or self.default
+    def transform(self, data: list[Transaction], _y=None):
+        """Return list of entry attributes."""
+        return [self._txn_getter(d) or self.default for d in data]
 
 
 class StringVectorizer(CountVectorizer):
@@ -62,26 +62,26 @@ class StringVectorizer(CountVectorizer):
     def __init__(self, tokenizer=None):
         super().__init__(ngram_range=(1, 3), tokenizer=tokenizer)
 
-    def fit_transform(self, raw_documents, y=None):
+    def fit_transform(self, raw_documents: list[str], y=None):
         try:
             return super().fit_transform(raw_documents, y)
         except ValueError:
             return numpy.zeros(shape=(len(raw_documents), 0))
 
-    def transform(self, raw_documents):
+    def transform(self, raw_documents: list[str], _y=None):
         try:
             return super().transform(raw_documents)
         except ValueError:
             return numpy.zeros(shape=(len(raw_documents), 0))
 
 
-def get_pipeline(attribute, tokenizer):
+def get_pipeline(attribute: str, tokenizer):
     """Make a pipeline for a given entry attribute."""
 
     if attribute.startswith("date."):
-        return make_pipeline(AttrGetter(attribute), ArrayCaster())
+        return NumericTxnAttribute(attribute)
 
     # Treat all other attributes as strings.
     return make_pipeline(
-        AttrGetter(attribute, ""), StringVectorizer(tokenizer)
+        AttrGetter(attribute, default=""), StringVectorizer(tokenizer)
     )
